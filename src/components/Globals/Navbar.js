dfbadf6a8a99a5ee3cf8411e692ea61af797bc6b
Waga Odongo <wagaodongo@gmail.com>
2019-03-18 16:30:43 +0300

    navbar
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- src/components/Globals/Navbar.js
+++ src/components/Globals/Navbar.js
@@ -1,8 +1,45 @@
 import React, { Component } from "react"
+import { Link } from "gatsby"
+import logo from "../../images/logo.svg"
+import { FaCartArrowDown } from "react-icons/fa"
 
 class Navbar extends Component {
+  state = {
+    navbarOpen: false,
+    css: "collapse navbar-collapse",
+    links: [
+      {
+        id: 1,
+        path: "/",
+        text: "Home",
+      },
+      {
+        id: 2,
+        path: "/about",
+        text: "About",
+      },
+    ],
+  }
+
+  navbarHandler = () => {
+    console.log("Hello")
+  }
   render() {
-    return <div> navbar content</div>
+    return (
+      <nav className="navbar navbar-expand-sm bg-light navbar-light">
+        {" "}
+        <Link to="/" className="navbar-brand">
+          <img src={logo} alt="logo" />
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={this.navbarHandler}
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+      </nav>
+    )
   }
 }
 
