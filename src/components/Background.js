d83275ffe3bfa6ef77a71e4b1d5355c63f07bbdb
Waga Odongo <wagaodongo@gmail.com>
2019-03-19 13:34:53 +0300

    bug squashed
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- src/components/Background.js
+++ src/components/Background.js
@@ -3,7 +3,7 @@ import BackgroundImage from "gatsby-background-image"
 
 const Background = ({ img, styleClass, title, children }) => {
   return (
-    <BackgroundImage clasName={styleClass} fluid={img}>
+    <BackgroundImage className={styleClass} fluid={img}>
       <h1 className="title text-white text-uppercase text-center display-4 font-weight-bold">
         {title}
       </h1>
