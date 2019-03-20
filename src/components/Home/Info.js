bda07a96ecca4cb96ba3a93faf8ba9695e08f0a3
Waga Odongo <wagaodongo@gmail.com>
2019-03-20 11:42:03 +0300

    footer
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- src/components/Home/Info.js
+++ src/components/Home/Info.js
@@ -1,31 +1,29 @@
 import React from "react"
 import { Link } from "gatsby"
-
-const Info = () => {
+import Title from "../Globals/Title"
+export default function Info() {
   return (
     <section className="py-5">
       <div className="container">
+        <Title title="our story" />
         <div className="row">
-          <div className=" col-10 col-sm-8 mx-auto text-center" />
-
-          <p className="lead text-muted mb-5">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi,
-            doloribus? Repellat, quae a numquam nulla soluta minus temporibus
-            suscipit asperiores esse aliquid deleniti ipsa nesciunt velit
-            obcaecati facere odit fugiat voluptas corrupti dolores qui excepturi
-            laudantium iste neque. Ipsa, nulla laborum! Architecto, incidunt?
-            Nisi doloremque eligendi laudantium, praesentium doloribus
-            molestiae!
-          </p>
-          <Link to="/About">
-            <button className="btn text-uppercase btn-yellow">
-              About Page
-            </button>
-          </Link>
+          <div className="col-10 col-sm-8 mx-auto text-center">
+            <p className="lead text-muted mb-5">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex sed,
+              ea error similique autem esse nesciunt explicabo quaerat quam,
+              doloremque possimus tenetur excepturi iusto laudantium tempora
+              voluptatum ipsam numquam consequatur vel blanditiis soluta velit
+              necessitatibus nemo? Dignissimos quis iure accusamus beatae modi,
+              autem expedita mollitia enim nobis laborum eos voluptatum.
+            </p>
+            <Link to="/About/">
+              <button className="btn text-uppercase btn-yellow">
+                about page
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
     </section>
   )
 }
-
-export default Info
