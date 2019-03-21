05901c44ebfb301dafc4f6984d496aa60741645d
Waga Odongo <wagaodongo@gmail.com>
2019-03-21 21:20:53 +0300

    menu.js
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

---
+++ src/components/Home/Menu.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react"
+import Title from "../Globals/Title"
+import Img from "gatsby-image"
+
+class Menu extends Component {
+  render() {
+    return <div>content from menu component </div>
+  }
+}
+
+export default Menu
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Layout from "../components/layout"
 
 import SEO from "../components/seo"
 import Background from "../components/Background"
-import Info from "../components/Home/Info"
+import Menu from "../components/Home/Menu"
 
 const IndexPage = ({ data }) => (
   <Layout>
@@ -16,15 +16,27 @@ const IndexPage = ({ data }) => (
       styleClass="default-background"
     />
     <Info />
+    <Menu />
   </Layout>
 )
 
 export const query = graphql`
   {
-    img: file(relativePath: { eq: "default-background.jpeg" }) {
-      childImageSharp {
-        fluid {
-          ...GatsbyImageSharpFluid_tracedSVG
+    menu: allContentfulCoffeeItem {
+      edges {
+        node {
+          id
+          title
+          description {
+            description
+          }
+          price
+          category
+          image {
+            fixed(width: 50, height: 50) {
+              ...GatsbyContentfulFixed_tracedSVG
+            }
+          }
         }
       }
     }
